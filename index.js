2354bbad6d2ae3516c1ab22e34a0415ddf1b82d1
Henrique de Sousa Barbase <henrique2029@hotmail.com>
2021-04-10 20:01:52 +0000

    add js

--- index.js
+++ index.js
@@ -4,5 +4,24 @@ document.addEventListener('DOMContentLoaded', () => {
     const displayCurrentPlayer = document.querySelector('#current-player')
     let currentPlayer = 1
 
+    for(let i = 0; i < squares.length; i++){
+        squares[i].onclick = () => {
+        //if the square below your current sqaure is taken, you can go ontop of it
+            if(square[i + 7].classList.contains('taken')) {
+                if(currentPlayer == 1) {
+                    squares[i].classList.add('taken')
+                    squares[i].classList.add('player-one')
+                    currentPlayer = 2
+                    displayCurrentPlayer.innerHTML = currentPlayer
+                } if (currentPlayer == 2){ 
+                    squares[i].classList.add('taken')
+                    squares[i].classList.add('player-two')
+                    currentPlayer= 1
+                    displayCurrentPlayer.innerHTML = currentPlayer
+                }
+            } alert('cant gohere')
+        }
+    }
+
     
 })
